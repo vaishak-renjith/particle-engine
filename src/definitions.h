3a052c0b6a68d4db2967ed0e32d3d39f390dc10e
skyler <skylerakt@gmail.com>
2024-06-25 22:24:44 -0700

    oops

--- src/definitions.h
+++ src/definitions.h
@@ -5,11 +5,7 @@
 const int SCREEN_WIDTH = 1000;
 const int SCREEN_HEIGHT = 1000;
 
-<<<<<<< HEAD
-const int PIXEL_SIZE = 10;
-=======
 const int PIXEL_SIZE = 2;
->>>>>>> af629a5 (clean code)
 const int BRUSH_RAD = 5;
 
 const int GRID_WIDTH = SCREEN_WIDTH / PIXEL_SIZE;
